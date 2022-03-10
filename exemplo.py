b2c53a5e72a17983f4539d9a56b0fe341d269e2c
Pedro Ferreira <sm8epedro18@gmail.com>
2022-03-10 15:48:53 +0000

    Criar um tuplo para a primeira linha
    Vai facilitar na leitura caso tenha arrays

--- exemplo.py
+++ exemplo.py
@@ -1,10 +1,17 @@
 import re
 import sys
 
-
+fWrite = open("ex.csv","w")
 f = open("emd.csv","r")
 g = f.readline()
 h = ""
+
+header = r"([\w ]+)((\{\d+\}|\{\d+,\d+\})(::\w+)?)?"
+is_valid = r"^((" + header + ")?,)+$"
+
+res = re.match(is_valid, g)
+lista = re.findall(header, g)
+
 if(re.search(r',.*\{.+\},',g)): #caso existam arrays para trabalhar
     print("1")
 
@@ -12,18 +19,28 @@ elif(re.search(r',.*\{.+\}.+,',g)): #caso existam arrays com funções de agrega
     print("2")
 
 else: #caso normal quando não existe arrays    
-    campos = g.split(',')
-    campos2 = campos[:-1]
-    
+    campos2 = lista[:-1]
+    lista[len(lista)-1][0][:-1] #tira o \n do ultimo elemento
     for linha2 in campos2:
-        h = h + "(?:(?P<" + linha2 + ">.*),)"
+        h = h + "(?:(?P<" + linha2[0] + ">.*),)"
+    h = h + "(?:(?P<" + lista[len(lista)-1][0] + ">.*))" 
     
-    h = h + "(?:(?P<" + campos[len(campos)-1][:-1] + ">.*))" 
-    
-    for linha in f:
+    fWrite.write("[\n") 
+    for linha in f:   
         maluno = re.search(h,linha)
-        if maluno:
-            dic = maluno.groupdict()
-            print(dic.get("index",0))
+        
+        if not maluno:
+            print("Erro na criação de dicionário")
+
         else:
-            print("Invalido")    
+            dic = maluno.groupdict()
+            fWrite.write("    {\n   ")
+            
+            for cam in lista: # escreve toda a informação entre aspas
+                fWrite.write("  \"" + cam[0] + "\" : \""+ dic.get(cam[0]) +"\"\n    ")
+            
+            fWrite.write("},\n")   
+
+    fWrite.write("]")           
+        
+            
