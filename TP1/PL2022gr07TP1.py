542073843e826a03e748539601f630032f327c2b
Catarina <a93259@alunos.uminho.pt>
2022-03-27 22:52:01 +0100

    Final

--- TP1/PL2022gr07TP1.py
+++ TP1/PL2022gr07TP1.py
@@ -4,13 +4,15 @@ fWrite = open("output.json","w")                                                
 fRead = open("input.csv","r")                                                                                   # abre ficheiro de leitura
 firstLine = fRead.readline()                                                                                    # recolhe a primeira linha
 
-campo = r"([\w]+|\"[\w,;]+\")((\{\d+\}|\{\d*,\d+\})(::\w+)?)?"                                                  # er para 1 campo
-cabecalho = r"^((" + campo + ")?[,;|\n])+$"                                                                      # er para cabeçalho
+campo = r"([\w]+|\"[\w,;|\s]+\")((\{\d+\}|\{\d*,\d+\})(::\w+)?)?"                                               # er para 1 campo
+cabecalho = r"^((" + campo + ")?[,;|\n])+$"                                                                     # er para cabeçalho
 
 validInfo = re.match(cabecalho, firstLine)                                                                      # guarda se cabecalho é valido
 listaCabecalho = re.findall(campo, firstLine)                                                                   # guarda lista de match objects de cada campo
-sizeListComa = len(re.split(',', firstLine))                                                                    # número de campos separados por ,
-sizeListBarra = len(re.split('|', firstLine))                                                                   # número de campos separados por |
+sizeListComa = len(re.split(r'\,', firstLine))                                                                  # número de campos separados por ,
+sizeListSemiColon = len(re.split(r'\;', firstLine))  
+sizeListBarra = len(re.split(r'\|', firstLine))                                                                 # número de campos separados por |
+split = re.split(r'[,;|\n]', firstLine)
 
 indCampo = 0                                                                                                    # indice que vai percorrer os campos de informação
 indCabecalho = 0                                                                                                # indice que vai percorrer os campos cabecalho   
@@ -30,45 +32,44 @@ def sortArray(valores):
 
 def limpaLinha(linhaSplit):
     listaLimpa = []  
-    concatenate = ""                                                     # onde vai ser guardados campos que pertencem juntos
+    concatenate = ""                                                    # onde vai ser guardados campos que pertencem juntos
     i = 0
 
-    while i < (len(linhaSplit)):                                         # enquanto os campos não acabarem
-        
-        if re.search(r'\".*\"',linhaSplit[i]):                           # se existirem 2 aspas no mesmo campo, o campo está correto                                             
+    while i < (len(linhaSplit)):                                        # enquanto os campos não acabarem
+        if re.search(r'\".*\"',linhaSplit[i]):                          # se existirem 2 aspas no mesmo campo, o campo está correto                                             
             listaLimpa.append(linhaSplit[i])
 
-        elif re.search(r'^\"',linhaSplit[i]):                            # se existir só 1 aspa no campo, o campo tem de ser completado
-            while linhaSplit[i][-1] != "\"":                             # enquanto não aparecer um campo a acabar com 1 aspas o campo é adicionado à string com o delimitador correto
-                if sizeListComa == len(linhaSplit):                      # se delimitador for ,
+        elif re.search(r'^\"',linhaSplit[i]):                           # se existir só 1 aspa no campo, o campo tem de ser completado
+            while linhaSplit[i][-1] != "\"":                            # enquanto não aparecer um campo a acabar com 1 aspas o campo é adicionado à string com o delimitador correto
+                if sizeListComa+1 == len(split):                        # se delimitador for ,
                     concatenate = concatenate + linhaSplit[i]+ ","
-                elif sizeListBarra == len(linhaSplit):                   # se delimitador for |
-                    concatenate = concatenate + linhaSplit[i]+ "|"
-                else:                                                    # se delimitador for ;
+                elif sizeListSemiColon+1 == len(split):                 # se delimitador for ;
                     concatenate = concatenate + linhaSplit[i]+ ";"
+                else:                                                   # se delimitador for |
+                    concatenate = concatenate + linhaSplit[i]+ "|"
                 i+=1
-            concatenate = concatenate + linhaSplit[i]                    # quando encontrar as aspas que faltam adiciona à string sem delimitador 
-            listaLimpa.append(concatenate)                               # campo adicionado à lista limpa
-
-        else:                                                            # se campo não tiver nenhuma aspas está correto
+            concatenate = concatenate + linhaSplit[i]                   # quando encontrar as aspas que faltam adiciona à string sem delimitador 
+            listaLimpa.append(concatenate[1:-1])                        # campo adicionado à lista limpa
+            
+        else:                                                           # se campo não tiver nenhuma aspas está correto
             listaLimpa.append(linhaSplit[i])
         i+=1
 
     return listaLimpa 
 
-def cleanValores(valores):
+def cleanValores(valores):                                                                                      # Cria uma lista de strings sem '
     cleaned = []
     for valor in valores:
         cleaned.append(str(valor).replace("'",""))
     return cleaned
 
-def myLength(e):
+def myLength(e):                                                                                                # função utilizada no sort pelo comprimento de um elemento
     return len(e)
 
 fWrite.write("[")   
 if validInfo:                                                                                                   # se o cabeçalho é válido avança
     for linha in fRead:                                                                                         # ciclo para cada uma das linhas de conteudo
-        linhaSplit = re.split('[,;|]', linha)                                                                    # guardar lista de campos separados por , ou ; para ser limpa
+        linhaSplit = re.split(r'[,;|\n]', linha)                                                                # guardar lista de campos separados por , ou ; para ser limpa
         listaLimpa = limpaLinha(linhaSplit)                                                                     # lista com correta posição de delimitadores (delimitadores dentro de strigs não contam como campo)
         
         if secondLine:                                                                                          # adiciona formato do JSON inicial
@@ -79,7 +80,7 @@ if validInfo:                                                                   
         while(indCabecalho < len(listaCabecalho)):                                                              # enquanto os campos não acabarem
             valores = []                                                                                        # guarda os valores das listas
             strings = []                                                                                        # guarda as strings das listas
-            listaTotal = []
+            listaTotal = []                                                                                     # guarda todo o conteudo da lista
 
             if intervaloVal := re.search(r'(?:\{(\d*),(\d+)\})|(?:\{(\d+)\})',listaCabecalho[indCabecalho][2]):     # se encontrar algum intervalo
             
@@ -91,15 +92,15 @@ if validInfo:                                                                   
                 
                 while intervaloFim > indCampo:                                                                      # Enquanto não chegar ao fim do intervalo:
     
-                    if indCampo >= len(listaLimpa) or (listaLimpa[indCampo] == "") or (listaLimpa[indCampo] == "\n"):     # se for o último elemento do intervalo ou da linha indice avança
-                        indCampo = intervaloFim - 1                                                                       # para o último valor do intervalo
+                    if indCampo >= len(listaLimpa) or (listaLimpa[indCampo] == "") or (listaLimpa[indCampo] == "\n"):   # se for o último elemento do intervalo ou da linha indice avança
+                        indCampo = intervaloFim - 1                                                                     # para o último valor do intervalo
 
-                    elif re.search(r'\d+',listaLimpa[indCampo]):                                                          # se não for vazio, vai adicionar o valor à lista dos guardados
-                        valores.append(listaLimpa[indCampo])
-                        listaTotal.append(listaLimpa[indCampo])
-                    else:
-                        strings.append("\""+listaLimpa[indCampo]+"\"")
-                        listaTotal.append("\""+listaLimpa[indCampo]+"\"")    
+                    elif re.search(r'\d+',listaLimpa[indCampo]):                                                        # se não for vazio e for um numero:
+                        valores.append(listaLimpa[indCampo])                                                            # vai ser adicionado à lista dos valores
+                        listaTotal.append(listaLimpa[indCampo])                                                         # vai ser adicionado à lista de todo o conteudo da lista
+                    else:                                                                                               # se não for vazio e for uma string
+                        strings.append("\""+listaLimpa[indCampo]+"\"")                                                  # vai ser adicionado à lista das strings
+                        listaTotal.append("\""+listaLimpa[indCampo]+"\"")                                               # vai ser adicionado à lista de todo o conteudo da lista
                     
                     indCampo = indCampo + 1                                                                     
 
@@ -112,15 +113,15 @@ if validInfo:                                                                   
                     soma = sumArray(valores)                                                                                        # soma todos os valores guardados
                     fWrite.write("\t\""+listaCabecalho[indCabecalho][0]+"_media\" : " + str(soma/len(valores)))                     # coloca no JSON com o prefixo_media
 
-                elif listaCabecalho[indCabecalho][3] == "::sort":                                                                   
-                    cleaned = cleanValores(listaTotal)
-                    cleaned.sort()                                                                                       
-                    fWrite.write("\t\""+listaCabecalho[indCabecalho][0]+"_sort\" : " + str(cleaned).replace("\'",""))
+                elif listaCabecalho[indCabecalho][3] == "::sort":                                                                   # se for uma função de sort           
+                    cleaned = cleanValores(listaTotal)                                                                              # vai retirar ' de todos os valores
+                    cleaned.sort()                                                                                                  # vai ordenar a lista de strings
+                    fWrite.write("\t\""+listaCabecalho[indCabecalho][0]+"_sort\" : " + str(cleaned).replace("\'",""))               
                 
-                elif listaCabecalho[indCabecalho][3] == "::sortByLength":                                                                 
-                    cleaned = cleanValores(listaTotal)
-                    cleaned.sort(key=myLength)                                                                                       
-                    fWrite.write("\t\""+listaCabecalho[indCabecalho][0]+"_sort\" : " + str(cleaned).replace("\'",""))
+                elif listaCabecalho[indCabecalho][3] == "::sortByLength":                                                           # se for uma função de sortByLength                                 
+                    cleaned = cleanValores(listaTotal)                                                                              # vai retirar ' de todos os valores
+                    cleaned.sort(key=myLength)                                                                                      # vai ordenar a lista de strings pelo comprimento do valor       
+                    fWrite.write("\t\""+listaCabecalho[indCabecalho][0]+"_sortByLength\" : " + str(cleaned).replace("\'",""))
 
                 else :
                     fWrite.write("\t\""+listaCabecalho[indCabecalho][0]+"\" : "+ str(listaTotal).replace("'","").replace("\\n","")) # se for apenas para apresentar os números vai retirar primeiro os ' e o \n se existir
@@ -128,8 +129,11 @@ if validInfo:                                                                   
                 indCabecalho = indCabecalho + 1     
             
             else:
-                listaLimpa[indCampo] = listaLimpa[indCampo].strip("\n")                                      # retira o \n do ultimo campo 
-                fWrite.write("\t\""+listaCabecalho[indCabecalho][0]+"\" : \"" + listaLimpa[indCampo]+"\"")   # escreve no ficheiro JSON o campo com o correspondente cabeçalho
+                listaLimpa[indCampo] = listaLimpa[indCampo].strip("\n")                                         # retira o \n do ultimo campo 
+                if listaCabecalho[indCabecalho][0][0] != "\"":                                                  # verifica se cabecalho já tem ""
+                    fWrite.write("\t\""+listaCabecalho[indCabecalho][0]+"\" : \"" + listaLimpa[indCampo]+"\"") 
+                else:
+                    fWrite.write("\t"+listaCabecalho[indCabecalho][0]+" : \"" + listaLimpa[indCampo]+"\"")      
                 indCampo = indCampo + 1
                 indCabecalho = indCabecalho + 1
         
